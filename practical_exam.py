95df451428e0ce31da9ebc7f479d99c0cfd9b4d9
JanaShebl <janamohamed195200@gmail.com>
2025-08-28 23:17:33 +0300

    Update practical_exam.py

--- practical_exam.py
+++ practical_exam.py
@@ -56,7 +56,7 @@ for emp_id, emp_info in employees.items():
     else:
         department_count[department] = 1
 print(department_count)'''
-#problem 1
+# problem 1
 """def count_of_even(numbers):
     evenNumber=0
     for num in numbers:
@@ -65,7 +65,7 @@ print(department_count)'''
     print(f"there are {evenNumber} even numbers")
 numbers=[52541,546,878,215]
 count_of_even(numbers)"""
-#problem 2
+# problem 2
 '''def sum_of_numbers(numbers):
     sum=0
     for i in numbers:
@@ -79,7 +79,7 @@ sum_of_numbers(numbers)'''
     print(f'{result}')
 numbers = [22, 43, 54, 5]
 get_sum(numbers)'''
-#problem 3
+# problem 3
 '''def find_negative_number(numbers):
     for num in numbers:
         if num<1:
@@ -88,7 +88,7 @@ get_sum(numbers)'''
     print('no negative numbers')
 numbers=[55,64,-54,548]
 find_negative_number(numbers)'''
-#problem 4
+# problem 4
 '''def count_great_numbers(numbers):
     count=0
     for num in numbers:
@@ -98,7 +98,7 @@ find_negative_number(numbers)'''
 numbers=[654,554,548,5489,995]
 threshold=700
 count_great_numbers(numbers)'''
-#problem 5
+# problem 5
 '''def find_the_largest_second_number(numbers):
     largest=max(numbers)
     numbers.remove(largest)
@@ -106,7 +106,7 @@ count_great_numbers(numbers)'''
     print(f"{largest2} is the second largest number")
 numbers=[6656,84,2105,65,5521]
 find_the_largest_second_number(numbers)'''
-#problem 6
+# problem 6
 '''def how_many_vowels(strings):
     count=0
     for char in strings:
@@ -115,12 +115,12 @@ find_the_largest_second_number(numbers)'''
     print(f"there is {count} in the string")
 strings="congratulations"
 how_many_vowels(strings)'''
-#problem 7
+# problem 7
 '''def reverse_a_string(x):
     y=x[::-1]
     print(f'the reserved string is {y}')
 reverse_a_string("python")'''
-#problem 8
+# problem 8
 """def find_all_words(words,letter):
     for specified in words:
         if specified.startswith(letter):
@@ -130,7 +130,7 @@ reverse_a_string("python")'''
 words=["apple", "banana", "cherry", "apricot", "blueberry"]
 letter ="a"
 find_all_words(words,letter)"""
-#problem 9
+# problem 9
 '''def Calculate_the_Length_of_Each_String(my_list):
     length=[]
     for x in my_list:
@@ -138,7 +138,7 @@ find_all_words(words,letter)"""
     print(length)
 my_list=['jana','mohamed', 'shebl']
 Calculate_the_Length_of_Each_String(my_list)'''
-#problem 10
+# problem 10
 '''def find_the_longest_word(words):
     longest=words[0]
     for x in words:
@@ -147,7 +147,7 @@ Calculate_the_Length_of_Each_String(my_list)'''
     print(f'the longest word is {longest}')
 words = ["apple", "banana", "cherry", "blueberry"]
 find_the_longest_word(words)'''
-#problem 11
+# problem 11
 '''def remove_specific_element(numbers , element):
     new_list=[]
     for x in numbers:
@@ -157,7 +157,7 @@ find_the_longest_word(words)'''
 numbers = [1, 2, 3, 2, 4, 2, 5]
 element = 2
 remove_specific_element(numbers,element) '''
-#problem 12
+# problem 12
 '''def all_string_in_sentence(words):
     sentence=''
     for x in words:
@@ -165,7 +165,7 @@ remove_specific_element(numbers,element) '''
     print(f' the new sentence is {sentence}')
 words= ["I", "love", "coding"]
 all_string_in_sentence(words)'''
-#problem 13
+# problem 13
 '''def Palindromes(words):
     for x in words:
         if x==x[::-1]:
@@ -174,7 +174,7 @@ all_string_in_sentence(words)'''
             print(f"{x} is not a palindrome")
 words=["madam", "hello", "racecar"]
 Palindromes(words)'''
-#problem 14
+# problem 14
 '''def factorial(n):
     result=1
     for i in range(1, n + 1):
@@ -182,9 +182,9 @@ Palindromes(words)'''
     print(result)
 n=2
 factorial(n)'''
-#problem 15
+# problem 15
 
-#problem 16
+# problem 16
 '''def average_of_numbers(numbers):
     average=1
     result=1
@@ -194,7 +194,7 @@ factorial(n)'''
     print(average)
 numbers = [10, 20, 30, 40]
 average_of_numbers(numbers)'''
-#problem 17
+# problem 17
 '''def how_many_words(sentence):
     count=0
     for x in sentence.split():
@@ -202,13 +202,13 @@ average_of_numbers(numbers)'''
     print(f'The sentence has {count} words.')
 sentence="I love programming"
 how_many_words(sentence)'''
-#problem 18
+# problem 18
 '''def reverse_elements(numbers):
     numbers.reverse()
     print(numbers)
 numbers = [1, 2, 3, 4]
 reverse_elements(numbers)'''
-#problem 19
+# problem 19
 '''def Check_for_Palindrome(string):
     if string==string[::-1]:
         print(f"{string} is a palidroom")
@@ -216,13 +216,13 @@ reverse_elements(numbers)'''
         print(f"{string} is not a palindrome")
 string="madam"
 Check_for_Palindrome(string)'''
-#problem 20
+# problem 20
 '''def find_the_largest(numbers):
     largest=max(numbers)
     print(f'the largest number is {largest}')
 numbers = [10, 25, 5, 90 ,150]
 find_the_largest(numbers)'''
-#the last problem finallyyyyyyy
+# the last problem finallyyyyyyy
 '''def calculate_area(radius):
     area=3.14* radius**2
     print(f'{area}')
@@ -233,4 +233,5 @@ calculate_area(radius)'''
     circumference=2*3.14*radius
     print(circumference)
 radius=5
+
 calculate_circumference(radius)'''
